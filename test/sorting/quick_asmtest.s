9ce89e3debc9311a18a3bafa76206341f99d4171
Marcelo Pinto <mpinto70@gmail.com>
2022-08-09 09:43:01 -0400

    Add location for check

--- test/sorting/quick_asmtest.s
+++ test/sorting/quick_asmtest.s
@@ -5,11 +5,12 @@
 main:
     stp     x29, x30, [sp, -16]!
 
-    mov     x0, 154
-    mov     x1, 256
+    mov     x0, 0xff0000000
+    mov     x1, 0xff8000000
     ldr     x8, =_right_pivot
+    ldr     x18, =main
     mov     x9, 1
-    mov     x10, 154
+    mov     x10, 0xffc000000
     bl      check_equal
 
     mov     x0, 0
@@ -90,6 +91,15 @@ main:
     print_stderr
 .endm
 
+// will put a thousand separator in address pointed by x3 - 1
+// will decrement x3, increment x2 and change x6
+.macro thousand_separator
+    sub     x3, x3, 1
+    add     x2, x2, 1
+    mov     x6, '\''
+    strb    w6, [x3]
+.endm
+
 // print integer value
 // @param x0    value to be printed
 _print_int:
@@ -100,17 +110,23 @@ _print_int:
     cbz     x0, .print_int.zero
 
     mov     x4, 10
+    mov     x7, 3
 
     .print_int.loop:
         cbz     x0, .print_int.str
         sub     x3, x3, 1
         add     x2, x2, 1
+        sub     x7, x7, 1
 
-        udiv    x5, x0, x4          // x5 = xN / 10
-        msub    x6, x5, x4, x0      // x6 = xN % 10 (the digit to be converted)
+        udiv    x5, x0, x4          // x5 = x0 / 10
+        msub    x6, x5, x4, x0      // x6 = x0 % 10 (the digit to be converted)
         add     x6, x6, '0'         // convert to char
         strb    w6, [x3]
         mov     x0, x5
+        cbnz    x7, .print_int.loop.no_thousand
+        mov     x7, 3
+        thousand_separator
+        .print_int.loop.no_thousand:
         b       .print_int.loop
 
     .print_int.zero:
@@ -131,6 +147,69 @@ _print_int:
     bl      _print_int
 .endm
 
+// print hex value
+// @param x0    value to be printed
+_print_hex:
+    mov     x2, 0                   // size
+    ldr     x3, =buffer             // write head into buffer (backwards)
+    ldr     x4, =buffer_len
+    add     x3, x3, x4
+    cbz     x0, .print_hex.zero
+
+    mov     x7, 4
+    .print_hex.loop:
+        cbz     x0, .print_hex.str
+        sub     x3, x3, 1
+        add     x2, x2, 1
+        sub     x7, x7, 1
+
+        and     x6, x0, 0xf
+        mov     x5, 10
+        cmp     x6, x5
+        b.ge    .print_hex.loop.if_hexa
+        mov     x4, '0'         // base digit
+        b       .print_hex.loop.if_hexa_end
+        .print_hex.loop.if_hexa:
+        mov     x4, 'a'         // base hexa
+        sub     x6, x6, 10
+        .print_hex.loop.if_hexa_end:
+
+        add     x6, x6, x4
+        strb    w6, [x3]
+        lsr     x0, x0, 4
+
+        cbnz    x7, .print_hex.loop.no_thousand
+        mov     x7, 4
+        thousand_separator
+        .print_hex.loop.no_thousand:
+
+        b       .print_hex.loop
+
+    .print_hex.zero:
+    sub     x3, x3, 1
+    mov     x6, '0'
+    strb    w6, [x3]
+    mov     x2, 1
+
+    .print_hex.str:
+    sub     x3, x3, 1
+    mov     x6, 'x'
+    strb    w6, [x3]
+    sub     x3, x3, 1
+    mov     x6, '0'
+    strb    w6, [x3]
+    add     x2, x2, 2       // space for '0x'
+    mov     x1, x3
+    // x2 already has the size
+    print_stderr
+    ret
+
+// print an int
+.macro print_hex xN
+    mov     x0, \xN
+    bl      _print_hex
+.endm
+
 // print a line break
 .macro print_ln
     ldr     x1, =line_break
@@ -143,6 +222,7 @@ _print_int:
 // @param x8        function to call
 // @param x9        bit mask for results set (bits 0-7)
 // @param x10 - x17 expected results
+// @param x18       base address
 check_equal:
     prepare_frame
 
@@ -161,12 +241,22 @@ check_equal:
         load_expected       x11, x9
         cmp                 x10, x11
         b.eq                .check_equal.loop_result.next
-        print_str           eq_error_0, eq_error_0_len
+        print_str           error_in_register, error_in_register_len
         print_int           x9
-        print_str           eq_error_1, eq_error_1_len
+        print_str           separator, separator_len
         print_int           x10
-        print_str           eq_error_2, eq_error_2_len
+        print_str           eq_error, eq_error_len
         print_int           x11
+        print_str           error_location, error_location_len
+        ldr                 x11, [sp, 8]        // return address of caller x30
+        sub                 x11, x11, 4         // move to calling
+        ldr                 x10, [sp, 176]      // base address of calling function x18
+        sub                 x11, x11, x10
+        print_hex           x11
+        print_str           left_paren, left_paren_len
+        lsr                 x11, x11, 2
+        print_int           x11
+        print_str           right_paren, right_paren_len
         print_ln
 
         .check_equal.loop_result.next:
@@ -183,13 +273,21 @@ buffer:         //23456789 123456789 123456789 12
 buffer_len = . - buffer
 line_break:
     .ascii      "\n"
-eq_error_0:
+error_in_register:
     .asciz      "Error found in register x"
-eq_error_0_len = . - eq_error_0
-eq_error_1:
-    .asciz      " / "
-eq_error_1_len = . - eq_error_1
-eq_error_2:
+error_in_register_len = . - error_in_register
+separator:
+    .asciz      " ==> "
+separator_len = . - separator
+eq_error:
     .asciz      " != "
-eq_error_2_len = . - eq_error_2
-
+eq_error_len = . - eq_error
+error_location:
+    .asciz      " <== offset "
+error_location_len = . - error_location
+left_paren:
+    .asciz      " ("
+left_paren_len = . - left_paren
+right_paren:
+    .asciz      ") "
+right_paren_len = . - right_paren
