13d2d5048ca9dc57878eb4421e8d5b0801f89deb
Marcelo Pinto <mpinto70@gmail.com>
2023-03-12 22:35:34 -0400

    Upgrade gtest version

--- cmake/UnitTest.cmake
+++ cmake/UnitTest.cmake
@@ -5,7 +5,7 @@
 FetchContent_Declare(
         googletest
         GIT_REPOSITORY https://github.com/google/googletest.git
-        GIT_TAG        release-1.11.0
+        GIT_TAG        v1.13.0
 )
 
 FetchContent_GetProperties(googletest)
